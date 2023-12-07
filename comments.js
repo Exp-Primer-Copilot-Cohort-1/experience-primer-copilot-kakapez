85d1077ede383069db66479bc8ee67a95c56f4ab
kaka <53079514+kakapez@users.noreply.github.com>
2023-12-07 08:10:18 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -2,6 +2,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const path = require('path');
 const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Define routes or middleware as needed
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
